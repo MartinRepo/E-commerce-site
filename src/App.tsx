564ec10cb45a281f636ca03a8f868ff19f8188a2
Martin <1191731324@qq.com>
2024-05-02 11:22:17 +0100

    indicator, sort, instock

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,10 @@ type Product = {
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [searchedProducts, setSearchedProducts] = useState<Product[]>(itemList);
+  const [inStockOnly, setInStockOnly] = useState<boolean>(false);
 
   // ===== Hooks =====
-  useEffect(() => updateSearchedProducts(), [searchTerm]);
+  useEffect(() => updateSearchedProducts(), [searchTerm, inStockOnly]);
 
   // ===== Basket management =====
   function showBasket(){
@@ -36,14 +37,38 @@ function App() {
   }
 
   // ===== Search =====
-  function updateSearchedProducts(){
-    let holderList: Product[] = itemList;
-
-    setSearchedProducts(holderList.filter((product: Product) =>
-      product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    ));
+  function updateSearchedProducts() {
+    let filteredItems = itemList.filter((product: Product) => {
+      return product.name.toLowerCase().includes(searchTerm.toLowerCase()) && (!inStockOnly || product.quantity > 0);
+    });
+    setSearchedProducts(filteredItems);
   }
 
+  // ===== Sort =====
+  function sortItems(sortBy: string) {
+    const sortedProducts = [...searchedProducts];
+  
+    sortedProducts.sort((a, b) => {
+      switch (sortBy) {
+        case "AtoZ":
+          return a.name.localeCompare(b.name);
+        case "ZtoA":
+          return b.name.localeCompare(a.name);
+        case "£LtoH":
+          return a.price - b.price;
+        case "£HtoL":
+          return b.price - a.price;
+        case "*LtoH":
+          return a.rating - b.rating;
+        case "*HtoL":
+          return b.rating - a.rating;
+        default:
+          return 0;
+      }
+    });
+  
+    setSearchedProducts(sortedProducts);
+  }  
  
 
   return (
@@ -65,7 +90,7 @@ function App() {
       <div id="search-bar">
         <input type="text" placeholder="Search..." onChange={changeEventObject => setSearchTerm(changeEventObject.target.value)}></input>
         <div id="control-area">
-          <select>
+          <select id="sort-select" onChange={e => sortItems(e.target.value)}>
             <option value="AtoZ">By name (A - Z)</option>
             <option value="ZtoA">By name (Z - A)</option>
             <option value="£LtoH">By price (low - high)</option>
@@ -73,11 +98,11 @@ function App() {
             <option value="*LtoH">By rating (low - high)</option>
             <option value="*HtoL">By rating (high - low)</option>
           </select>
-          <input id="inStock" type="checkbox"></input>
+          <input id="inStock" type="checkbox" checked={inStockOnly} onChange={() => setInStockOnly(!inStockOnly)}></input>
           <label htmlFor="inStock">In stock</label>
         </div>
       </div>
-      <p id="results-indicator"></p>
+      <p id="results-indicator">{searchedProducts.length} products available</p>
       <ProductList itemList={searchedProducts}/>
     </div>
   )
